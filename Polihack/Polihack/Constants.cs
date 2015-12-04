78e2369b129219e881d7926dd888d68bb749bcb8
rpreda <rares.preda98@gmail.com>
2015-12-04 23:44:26 +0200

    added constants class, containing the enum for the web types atm

---
+++ Polihack/Polihack/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Polihack
+{
+    class Constants
+    {
+        public static enum WebTypes
+        {
+            Video,
+            Image,
+            Link,
+            Text
+        }
+    }
+}
