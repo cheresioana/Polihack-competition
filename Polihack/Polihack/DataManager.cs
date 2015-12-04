6667b1c2f1d7f04d0a404ef4a48cc42f6fa6daad
rpreda <rares.preda98@gmail.com>
2015-12-04 22:14:35 +0200

    DataManager work started

---
+++ Polihack/Polihack/DataManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Polihack
+{
+    class DataManager
+    {
+        public string rootPath;
+        public int error_code;
+        public DataManager(string rootPath)
+        {
+            this.rootPath = rootPath;
+            if (Directory.Exists(rootPath))
+                this.error_code = 0;
+            else
+                this.error_code = 1;
+        }
+        public bool entry_created(string ID)
+        {
+            return (Directory.Exists(Path.Combine(rootPath, ID)));
+        }
+    }
+}
