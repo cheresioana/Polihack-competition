0c82f525430e4c49116cf1576dfb96bbe596763d
rpreda <rares.preda98@gmail.com>
2015-12-05 10:25:46 +0200

    before correct

--- Polihack/Polihack/Picture.cs
+++ Polihack/Polihack/Picture.cs
@@ -7,15 +7,9 @@ namespace Polihack
 {
     class Picture
     {
-          struct info
-        {
-            public string   name;
-            public string   type;
-        }
-
         private int     _id;
         private string  _name;
-        private string  _type;
+        private Constants.SubTypes  _type;
         private Constants.SubTypes _subtype;
 
         public int Id
@@ -30,7 +24,7 @@ namespace Polihack
             set { }
         }
 
-        public string Type
+        public Constants.SubTypes Type
         {
             get { return _type; }
             set {}
@@ -44,13 +38,12 @@ namespace Polihack
 
         public Picture(int id, Constants.SubTypes subtype,Constants.MainTypes main_type )
         {
-            info info = new info();
             Csv csv = new Csv(id, subtype, main_type);
 
             _id = id;
             _subtype = subtype;
-            _name = csv.get_data_name("img");
-            _type = csv.get_data_type("img");
+            _name = csv.get_data_name(Constants.SubTypes.Img.ToString());
+            _type = csv.get_data_type(Constants.SubTypes.Img.ToString());
         }
     }
 }
