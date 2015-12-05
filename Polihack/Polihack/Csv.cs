77910b35f267eb2162fdd3995b34569bbaa11c9c
tusaracu <tudorsaracut@yahoo.com>
2015-12-05 02:24:37 +0200

    combined git pls don't fuck up arigatou senpaiii <3

---
+++ Polihack/Polihack/Csv.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace Polihack
+{
+    class Csv
+    {
+        public struct info
+        {
+            public string _name;
+            public string _type;
+        }
+
+        public info[] infos;
+
+        public Csv(Int64 id, Constants.SubTypes sub_type, Constants.MainTypes main_type)
+        {
+            DataManager manager = new DataManager(Constants.root_path, main_type);
+            string data = manager.entry_csv_contents(sub_type, id);
+            string[] rows;
+            string[] col;
+            int i = 0;
+
+            rows = data.Split('\n');
+            foreach (string s in rows)
+            {
+                col = s.Split(';');
+                infos[i] = new info();
+                infos[i]._name = col[0];
+                infos[i]._type = col[1];
+                i++;
+
+            }
+        }
+
+        public string get_data_name(string data_requested)
+        {
+            foreach (info info in infos)
+            {
+                if (info._type == data_requested)
+                    return info._name;
+            }
+            return null;
+        }
+        public string get_data_type(string data_requested)
+        {
+            foreach (info info in infos)
+            {
+                if (info._type == data_requested)
+                    return info._type;
+            }
+            return null;
+        }
+    }
+}
---
+++ Polihack/Polihack/Picture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Polihack
+{
+    class Picture
+    {
+          struct info
+        {
+            public string   name;
+            public string   type;
+        }
+
+        private int     _id;
+        private string  _name;
+        private string  _type;
+        private Constants.SubTypes _subtype;
+
+        public int Id
+        {
+            get { return _id; }
+            set { }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set {}
+        }
+
+        public Constants.SubTypes Subtype
+        {
+            get { return _subtype; }
+            set { }
+        }
+
+        public Picture(int id, Constants.SubTypes subtype,Constants.MainTypes main_type )
+        {
+            info info = new info();
+            Csv csv = new Csv(id, subtype, main_type);
+
+            _id = id;
+            _subtype = subtype;
+            _name = csv.get_data_name("img");
+            _type = csv.get_data_type("img");
+        }
+    }
+}
