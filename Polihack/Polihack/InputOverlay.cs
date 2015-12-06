f1c54a50849a78a265c38b3a5439013574d650e1
rpreda <rares.preda98@gmail.com>
2015-12-06 10:17:14 +0200

    input parsing, fixed frontend bug where radio button wasan't checked by default

--- Polihack/Polihack/InputOverlay.cs
+++ Polihack/Polihack/InputOverlay.cs
@@ -13,10 +13,21 @@ namespace Polihack
     {
         public Image screenshot;
         public Constants.SubTypes type;
+
+
+        private string filter_input(string raw_input)
+        {
+            raw_input = raw_input.Replace(Environment.NewLine, " ");
+            raw_input = raw_input.Replace(";", ",");
+            raw_input = raw_input.Replace("\n", " ");
+            return (raw_input);
+        }
+
         public InputOverlay()
         {
             InitializeComponent();
             type = Constants.SubTypes.Link;
+            radioButton1.Checked = true;
             this.TopMost = true;
         }
 
@@ -34,10 +45,14 @@ namespace Polihack
                     type = Constants.SubTypes.Vid;
                 DataManager manager = new DataManager(Constants.root_path, Constants.MainTypes.web);
                 Int64 lastid = manager.last_id_get(type);
-                manager.add_subType_csv(type, textBox1.Text, lastid + 1);//added this, pls work
+
+                manager.add_subType_csv(type, filter_input(textBox1.Text), lastid + 1);//used for search indexing
+
                 manager.last_id_set(type, lastid + 1);
                 manager.create_entry(type, lastid + 1);
-                manager.add_entry_data(Constants.DataType.text, type, lastid + 1, "title_desc.txt", textBox1.Text + ";" + textBox2.Text + ";" + textBox3.Text);
+                manager.add_entry_data(Constants.DataType.text, type, lastid + 1, "title_desc.txt",
+                    filter_input(textBox1.Text) + ";" + filter_input(textBox2.Text) + ";" + filter_input(textBox3.Text));
+
                 manager.add_entry_data(Constants.DataType.image, type, lastid + 1, "ss.png", screenshot as object);
                 this.Close();
             }
