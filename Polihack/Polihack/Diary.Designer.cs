a46b3d202d4b9e7281c0992d155d1fbbb568597b
Ioana <cheresioana@gmail.com>
2015-12-06 14:14:30 +0200

    da

--- Polihack/Polihack/Diary.Designer.cs
+++ Polihack/Polihack/Diary.Designer.cs
@@ -33,15 +33,21 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(259, 8);
+            this.button1.Location = new System.Drawing.Point(346, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.Size = new System.Drawing.Size(56, 30);
             this.button1.TabIndex = 1;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,71 +56,137 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.textBox1.Location = new System.Drawing.Point(78, 9);
+            this.textBox1.Location = new System.Drawing.Point(207, 11);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.Size = new System.Drawing.Size(124, 20);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Location = new System.Drawing.Point(8, 51);
+            this.textBox2.Location = new System.Drawing.Point(6, 41);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(905, 417);
+            this.textBox2.Size = new System.Drawing.Size(528, 339);
             this.textBox2.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Location = new System.Drawing.Point(94, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.Size = new System.Drawing.Size(52, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Save as: ";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(842, 6);
+            this.button2.Location = new System.Drawing.Point(478, 456);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 39);
+            this.button2.Size = new System.Drawing.Size(56, 32);
             this.button2.TabIndex = 5;
             this.button2.Text = "Theme";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(405, 6);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 17);
-            this.label2.TabIndex = 6;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(761, 6);
+            this.button3.Location = new System.Drawing.Point(11, 11);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 39);
+            this.button3.Size = new System.Drawing.Size(56, 32);
             this.button3.TabIndex = 7;
             this.button3.Text = "Back";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(78)))), ((int)(((byte)(78)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(11, 40);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(359, 122);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(11, 178);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(359, 122);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(11, 366);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(359, 122);
+            this.textBox5.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(476, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 28);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "New";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(169, 467);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 20);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(240, 467);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 20);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Diary
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
-            this.ClientSize = new System.Drawing.Size(929, 475);
+            this.ClientSize = new System.Drawing.Size(576, 499);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Diary";
             this.Text = "Diary";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Diary_FormClosing);
@@ -131,7 +203,12 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- Polihack/Polihack/Diary.cs
+++ Polihack/Polihack/Diary.cs
@@ -13,35 +13,103 @@ namespace Polihack
     {
       
         public string s;
+        int id = 1;
         public bool first = true;
         public Diary(string theme)
         {
             InitializeComponent();
-            label2.Text = theme;
-            label2.ForeColor = textBox1.BackColor;
             s = theme;
-             
-           // theme_dark();
         }
        
         public Diary()
         {
             InitializeComponent();
+
+        }
+
+        public void Load_data(int start)
+        {
+            
+            int i;
+            Constants.SubTypes sub_type = Constants.SubTypes.Text;
+            DataManager data_manager = new DataManager(Constants.root_path, Constants.MainTypes.text);
+           
+           
+            if (!(data_manager.entry_exists(sub_type, id + 3)))
+                button6.Hide();
+            else button6.Show();
+            if (id == 1)
+                button5.Hide();
+            else button5.Show();
+
+            for (i = 0; i < 3 ; i++)
+            {
+               
+                if (data_manager.entry_exists(sub_type, start + i))
+                {
+                    
+                    string data = data_manager.entry_data(sub_type, Constants.DataType.text, start + i, "title_desc.txt") as string;
+              
+                    data = data.Replace(";", System.Environment.NewLine);
+                    switch (i)
+                    {
+                        case 0:
+                            {
+                                textBox3.Text = data;
+                                break;
+                            }
+                        case 1:
+                            {
+                                textBox4.Text = data;
+                                break;
+                            }
+                        case 2:
+                            {
+                                textBox5.Text = data;
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            {
+                                textBox3.Hide();
+                                break;
+                            }
+                        case 1:
+                            {
+                                textBox4.Hide();
+                                break;
+                            }
+                        case 2:
+                            {
+                                textBox5.Hide();
+                                break;
+                            }
+                    }
+                }
+             }
         }
        
         private void button1_Click(object sender, EventArgs e)
         {
-            StringBuilder g =  new StringBuilder();
-            string c;
-                c = textBox1.Text;
-                
-            using (System.IO.StreamWriter file =
-           new System.IO.StreamWriter(Constants.root_path + "\\"+c + ".txt"))
-            {
-                string s;
-                s = label2.Text;
-                file.Write(s);
-            }
+            Constants.SubTypes type = Constants.SubTypes.Text;
+
+            DataManager manager = new DataManager(Constants.root_path, Constants.MainTypes.text);
+            Int64 lastid = manager.last_id_get(type);
+            
+            manager.add_subType_csv(type, textBox1.Text, lastid + 1);//used for search indexing
+
+            manager.last_id_set(type, lastid + 1);
+            manager.create_entry(type, lastid + 1);
+            manager.add_entry_data(Constants.DataType.text, type, lastid + 1, "title_desc.txt",
+                textBox1.Text + ";" + textBox2.Text);
+            textBox1.Text = null;
+            textBox2.Text = null;
+            main_page();
         }
         public void theme_dark()
         {
@@ -71,8 +139,41 @@ namespace Polihack
                 btn.FlatAppearance.BorderColor = Color.Gray;
                 btn.FlatAppearance.BorderSize = 1;
                 label1.ForeColor = Color.White;
-                textBox2.BackColor = Color.FromArgb(33, 33, 33);
-                textBox1.BackColor = Color.FromArgb(33, 33, 33);
+                //textBox2.BackColor = Color.FromArgb(33, 33, 33);
+                //textBox1.BackColor = Color.FromArgb(33, 33, 33);
+                textBox2.ForeColor = Color.White;
+                textBox3.ForeColor = Color.White;
+                textBox4.ForeColor = Color.White;
+                textBox5.ForeColor = Color.White;
+                textBox2.BackColor = Color.Black;
+                textBox3.BackColor = Color.Black;
+                textBox4.BackColor = Color.Black;
+                textBox5.BackColor = Color.Black;
+                btn = button4;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.BackColor = Color.FromArgb(33, 33, 33); ;
+                btn.ForeColor = Color.Gray;
+                btn.FlatAppearance.BorderColor = Color.Gray;
+                btn.FlatAppearance.BorderSize = 1;
+                label1.ForeColor = Color.White;
+                btn = button5;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.BackColor = Color.FromArgb(33, 33, 33); ;
+                btn.ForeColor = Color.Gray;
+                btn.FlatAppearance.BorderColor = Color.Gray;
+                btn.FlatAppearance.BorderSize = 1;
+                label1.ForeColor = Color.White;
+                btn = button6;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.BackColor = Color.FromArgb(33, 33, 33); ;
+                btn.ForeColor = Color.Gray;
+                btn.FlatAppearance.BorderColor = Color.Gray;
+                btn.FlatAppearance.BorderSize = 1;
+                label1.ForeColor = Color.White;
+                textBox3.BackColor = Color.Gray;
+                textBox4.BackColor = Color.Gray;
+                textBox5.BackColor = Color.Gray;
+              
                 first = false;
                // label2.Text = s;
                // label2.ForeColor = textBox1.BackColor;
@@ -112,6 +213,32 @@ namespace Polihack
                 //label2.Text = s;
                 //label2.ForeColor = textBox1.BackColor;
                 first = false;
+                textBox2.ForeColor = Color.Black;
+                textBox3.ForeColor = Color.Black;
+                textBox4.ForeColor = Color.Black;
+                textBox5.ForeColor = Color.Black;
+                textBox2.BackColor = Color.White;
+                textBox3.BackColor = Color.White;
+                textBox4.BackColor = Color.White;
+                textBox5.BackColor = Color.White;
+                btn = button4;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.BackColor = Color.White;
+                btn.ForeColor = Color.Black;
+                btn.FlatAppearance.BorderColor = Color.Aquamarine;
+                btn.FlatAppearance.BorderSize = 1;
+                btn = button5;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.BackColor = Color.White;
+                btn.ForeColor = Color.Black;
+                btn.FlatAppearance.BorderColor = Color.Aquamarine;
+                btn.FlatAppearance.BorderSize = 1;
+                btn = button6;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.BackColor = Color.White;
+                btn.ForeColor = Color.Black;
+                btn.FlatAppearance.BorderColor = Color.Aquamarine;
+                btn.FlatAppearance.BorderSize = 1;
            // }
 
         }
@@ -134,14 +261,42 @@ namespace Polihack
             textBox1.Text = aux1;
         }
 
+        public void main_page()
+        {
+            label1.Hide();
+            textBox1.Hide();
+            textBox2.Hide();
+            button1.Hide();
+            button4.Show();
+            textBox3.Show();
+            textBox4.Show();
+            textBox5.Show();
+            Load_data(id);
+
+        }
+
+        public void second_page()
+        {
+            label1.Show();
+            textBox1.Show();
+            textBox2.Show();
+            button1.Show();
+            button4.Hide();
+            textBox3.Hide();
+            textBox4.Hide();
+            textBox5.Hide();
+            button5.Hide();
+            button6.Hide();
+        }
+
         private void Diary_Load(object sender, EventArgs e)
         {
-            if (s == "dark")
+           if (s == "dark")
                 theme_dark();
            if (first ==  true)
                theme_light();
-           label2.Hide();
-            
+           main_page();
+           
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -149,6 +304,23 @@ namespace Polihack
             this.Hide();
             Form1 bec = new Form1();
             bec.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            second_page();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            id += 3;
+            Load_data(id);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            id -= 3;
+            Load_data(id);
         }
 
     }
